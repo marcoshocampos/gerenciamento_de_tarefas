c5a5afadf65afe7620ffd7bd462636e08726a7e5
Marcos Henrique <nick.oliveira.campos@gmail.com>
2024-09-15 19:42:14 -0300

    set environment variables

--- projeto_cadastro_tarefas/projeto_cadastro_tarefas/urls.py
+++ projeto_cadastro_tarefas/projeto_cadastro_tarefas/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from app_cadastro_tarefas import views
 
@@ -5,3 +7,9 @@ urlpatterns = [
     #rota, view responsável, nome de referência
     path('', views.cadastro, name='cadastro')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
